8c3bbca9bf563167ee6c1cb8debea1ea91a51a1a
Fernando Soares <fernandosoares.dev@gmail.com>
2022-07-15 13:30:20 -0300

    Ingressos

--- partials/page/tickets.php
+++ partials/page/tickets.php
@@ -1,5 +1,6 @@
 <section id="tickets">
     <div class="container">
+
         <div class="row">
             <div class="col-12">
                 <h2>Escolha seu ingresso</h2>
@@ -9,7 +10,7 @@
             <div class="col-12">
 
                 <div class="table-responsive">
-                    <table class="table table-dark table-striped stages">
+                    <table class="table table-dark stages">
                         <thead>
                             <tr>
                                 <th scope="col" width="60%"></th>
@@ -31,26 +32,197 @@
                             </tr>
                         </thead>
                         <tbody>
-                            <tr class="stage one">
-                                <th scope=" row">1</th>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
+
+                            <tr>
+                                <td colspan="4">
+                                    <table class="table ticket-info one">
+                                        <tbody>
+                                            <tr>
+                                                <td colspan="4">
+                                                    <p>Fase 1 <span>(até 23.08)</span></p>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <th width="60%" scope="row">Associado</th>
+                                                <td width="13.333333%" align="center">R$ 3.270</td>
+                                                <td width="13.333333%" align="center">R$ 2.000</td>
+                                                <td width="13.333333%" align="center">R$ 4.389</td>
+                                            </tr>
+                                            <tr>
+                                                <th width="60%" scope="row">Não associado</th>
+                                                <td width="13.333333%" align="center">R$ 5.050</td>
+                                                <td width="13.333333%" align="center">R$ 3.000</td>
+                                                <td width="13.333333%" align="center">R$ 6.589</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </td>
                             </tr>
-                            <tr class="stage two">
-                                <th scope="row">2</th>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
-                                <td>@fat</td>
+
+                            <tr>
+                                <td colspan="4">
+                                    <table class="table ticket-info one">
+                                        <tbody>
+                                            <tr>
+                                                <td colspan="4">
+                                                    <p>Fase 2 <span>(24.08 a 18.10)</span></p>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <th width="60%" scope="row">Associado</th>
+                                                <td width="13.333333%" align="center">R$ 3.270</td>
+                                                <td width="13.333333%" align="center">R$ 2.000</td>
+                                                <td width="13.333333%" align="center">R$ 4.389</td>
+                                            </tr>
+                                            <tr>
+                                                <th width="60%" scope="row">Não associado</th>
+                                                <td width="13.333333%" align="center">R$ 5.050</td>
+                                                <td width="13.333333%" align="center">R$ 3.000</td>
+                                                <td width="13.333333%" align="center">R$ 6.589</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </td>
                             </tr>
-                            <tr class="stage three">
-                                <th scope="row">3</th>
-                                <td colspan="2">Larry the Bird</td>
-                                <td>@twitter</td>
+
+                            <tr>
+                                <td colspan="4">
+                                    <table class="table ticket-info one">
+                                        <tbody>
+                                            <tr>
+                                                <td colspan="4">
+                                                    <p>Fase 1 <span>(a partir de 19.10)</span></p>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <th width="60%" scope="row">Associado</th>
+                                                <td width="13.333333%" align="center">R$ 3.270</td>
+                                                <td width="13.333333%" align="center">R$ 2.000</td>
+                                                <td width="13.333333%" align="center">R$ 4.389</td>
+                                            </tr>
+                                            <tr>
+                                                <th width="60%" scope="row">Não associado</th>
+                                                <td width="13.333333%" align="center">R$ 5.050</td>
+                                                <td width="13.333333%" align="center">R$ 3.000</td>
+                                                <td width="13.333333%" align="center">R$ 6.589</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </td>
                             </tr>
+
+                            <tr>
+                                <td colspan="4" class="buy-buttons">
+                                    <table class="table">
+                                        <tbody>
+                                            <tr>
+                                                <td>
+                                                    <small>* Preços congelados de 2019</small>
+                                                </td>
+                                                <td width="13.333333%" align="center">
+                                                    <a href="" class="presencial">Comprar</a>
+                                                </td>
+                                                <td width="13.333333%" align="center">
+                                                    <a href="" class="on-line">Comprar</a>
+                                                </td>
+                                                <td width="13.333333%" align="center">
+                                                    <a href="" class="meeting">Comprar</a>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </td>
+                            </tr>
+
                         </tbody>
                     </table>
                 </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-12">
+                <h2 class="second">Tipos de ingresso</h2>
+            </div>
+        </div>
+
+        <div class="row">
+
+            <div class="col-12 col-md-4">
+                <div class="ticket-type presencial">
+                    <div class="flag"></div>
+                    <h3>Congresso<br>Presencial</h3>
+                    <ul>
+                        <li class="enabled">Acesso físico ao Congresso</li>
+                        <li class="enabled">Acesso ao app do Congresso</li>
+                        <li class="enabled">Palestras nacionais e internacionais</li>
+                        <li class="enabled">Sessões plenárias </li>
+                        <li class="enabled">Breakout rooms</li>
+                        <li class="enabled"><em>Masterclasses</em></li>
+                        <li>Almoços-palestra exclusivos</li>
+                        <li class="enabled"><em>Working lunch</em></li>
+                        <li class="enabled">Feira de negócios presencial</li>
+                        <li class="enabled">Feira de negócios virtual</li>
+                        <li class="enabled">Espaço de networking</li>
+                        <li class="enabled">Acesso ao conteúdo gravado</li>
+                        <li class="enabled">Kit de congressista</li>
+                        <li class="enabled">Certificado de participação</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-4">
+                <div class="ticket-type on-line">
+                    <div class="flag"></div>
+                    <h3>Congresso<br>On-line</h3>
+                    <ul>
+                        <li>Acesso físico ao Congresso</li>
+                        <li class="enabled">Acesso ao app do Congresso</li>
+                        <li class="enabled">Palestras nacionais e internacionais</li>
+                        <li class="enabled">Sessões plenárias </li>
+                        <li>Breakout rooms</li>
+                        <li><em>Masterclasses</em></li>
+                        <li>Almoços-palestra exclusivos</li>
+                        <li><em>Working lunch</em></li>
+                        <li>Feira de negócios presencial</li>
+                        <li class="enabled">Feira de negócios virtual</li>
+                        <li>Espaço de networking</li>
+                        <li class="enabled">Acesso ao conteúdo gravado</li>
+                        <li>Kit de congressista</li>
+                        <li class="enabled">Certificado de participação</li>
+                    </ul>
+                </div>
+
+            </div>
+
+            <div class="col-12 col-md-4">
+                <div class="ticket-type meeting">
+                    <div class="flag"></div>
+                    <h3>Congresso +<br>Encontro</h3>
+                    <ul>
+                        <li class="enabled">Acesso físico ao Congresso</li>
+                        <li class="enabled">Acesso ao app do Congresso</li>
+                        <li class="enabled">Palestras nacionais e internacionais</li>
+                        <li class="enabled">Sessões plenárias </li>
+                        <li class="enabled">Breakout rooms</li>
+                        <li class="enabled"><em>Masterclasses</em></li>
+                        <li class="enabled">Almoços-palestra exclusivos</li>
+                        <li><em>Working lunch</em></li>
+                        <li class="enabled">Feira de negócios presencial</li>
+                        <li class="enabled">Feira de negócios virtual</li>
+                        <li class="enabled">Espaço de networking</li>
+                        <li class="enabled">Acesso ao conteúdo gravado</li>
+                        <li class="enabled">Kit de congressista</li>
+                        <li class="enabled">Certificado de participação</li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="row">
+            <div class="col-12 col-md-8 inscricao-corporativa">
+                <a class="d-block text-center" href="#">Inscrição corporativa. Solicite sua proposta</a>
             </div>
         </div>
 
