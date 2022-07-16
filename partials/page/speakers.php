d31657ec2d407697088b6fa28e7f75e78b348a07
Fernando Soares <fernandosoares.dev@gmail.com>
2022-07-16 11:07:31 -0300

    Links

--- partials/page/speakers.php
+++ partials/page/speakers.php
@@ -7,36 +7,46 @@
                     <div class="palestrante">
                         <img src="<?= get_template_directory_uri() . '/assets/img/almir.jpg' ?>" alt="Almir Narayamoga Surui" />
                         <div class="info">
-                            <h3>Almir Narayamoga Surui</h3>
-                            <p>Cargo</p>
+                            <a href="https://www.linkedin.com/in/almir-suru%C3%AD-79618420/" target="_blank">
+                                <h3>Almir Narayamoga Surui</h3>
+                                <p>Cargo</p>
+                            </a>
                         </div>
                     </div>
                     <div class="palestrante">
                         <img src="<?= get_template_directory_uri() . '/assets/img/ana-cristina.jpg' ?>" alt="Ana Cristina Cabral Gardner" />
                         <div class="info">
-                            <h3>Ana Cristina Cabral Gardner</h3>
-                            <p>Cargo</p>
+                            <a href="https://www.linkedin.com/in/anaccabral/" target="_blank">
+                                <h3>Ana Cristina Cabral Gardner</h3>
+                                <p>Cargo</p>
+                            </a>
                         </div>
                     </div>
                     <div class="palestrante">
                         <img src="<?= get_template_directory_uri() . '/assets/img/cezar.jpg' ?>" alt="Cezar Taurion" />
                         <div class="info">
-                            <h3>Cezar Taurion</h3>
-                            <p>Cargo</p>
+                            <a href="" target="_blank">
+                                <h3>Cezar Taurion</h3>
+                                <p>Cargo</p>
+                            </a>
                         </div>
                     </div>
                     <div class="palestrante">
                         <img src="<?= get_template_directory_uri() . '/assets/img/david.jpg' ?>" alt="David Beatty" />
                         <div class="info">
-                            <h3>David Beatty</h3>
-                            <p>Cargo</p>
+                            <a href="" target="_blank">
+                                <h3>David Beatty</h3>
+                                <p>Cargo</p>
+                            </a>
                         </div>
                     </div>
                     <div class="palestrante">
                         <img src="<?= get_template_directory_uri() . '/assets/img/pierri.jpg' ?>" alt="Pierre Lucena" />
                         <div class="info">
-                            <h3>Pierre Lucena</h3>
-                            <p>Cargo</p>
+                            <a href="https://www.linkedin.com/in/pierrelucena/" target="_blank">
+                                <h3>Pierre Lucena</h3>
+                                <p>Cargo</p>
+                            </a>
                         </div>
                     </div>
                 </div>
