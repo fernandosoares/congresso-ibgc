7339123e3bb7737286108c2729f3b5610fa0574d
Fernando Soares <fernandosoares.dev@gmail.com>
2022-10-14 09:11:06 -0300

    Patrocinadores atualizados

--- partials/page/sponsors.php
+++ partials/page/sponsors.php
@@ -18,6 +18,7 @@
                 <h4>Ouro</h4>
                 <img src="<?= get_template_directory_uri() . '/assets/img/ouro-b3.png' ?>" alt="B3" />
                 <img src="<?= get_template_directory_uri() . '/assets/img/ouro-sebrae.png' ?>" alt="Sebrae" />
+                <img src="<?= get_template_directory_uri() . '/assets/img/ouro-vale.png' ?>" alt="VALE" />
             </div>
         </div>
         <div class="row prata">
@@ -32,6 +33,8 @@
                 <h4>Bronze</h4>
                 <img src="<?= get_template_directory_uri() . '/assets/img/bronze-aws.png' ?>" alt="AWS" />
                 <img src="<?= get_template_directory_uri() . '/assets/img/bronze-bradesco.png' ?>" alt="BRADESCO" />
+                <img src="<?= get_template_directory_uri() . '/assets/img/bronze-cambridge.png' ?>" alt="CAMBIRDGE" />
+                <img src="<?= get_template_directory_uri() . '/assets/img/bronze-embraer.png' ?>" alt="EMBRAER" />
                 <img src="<?= get_template_directory_uri() . '/assets/img/bronze-mxm.png' ?>" alt="MXM" />
             </div>
         </div>
@@ -39,13 +42,13 @@
             <div class="col-12 text-center">
                 <h4>Plus</h4>
                 <img src="<?= get_template_directory_uri() . '/assets/img/plus-accountify.png' ?>" alt="ACCOUNTIFY" />
-                <img src="<?= get_template_directory_uri() . '/assets/img/plus-Blueshift.png' ?>" alt="BLUESHIFT" />
+                <img src="<?= get_template_directory_uri() . '/assets/img/plus-blueshift.png' ?>" alt="BLUESHIFT" />
                 <img src="<?= get_template_directory_uri() . '/assets/img/plus-cielo.png' ?>" alt="CIELO" />
                 <img src="<?= get_template_directory_uri() . '/assets/img/plus-exec.png' ?>" alt="EXEC" />
                 <img src="<?= get_template_directory_uri() . '/assets/img/plus-flow.png' ?>" alt="FLOW" />
                 <img src="<?= get_template_directory_uri() . '/assets/img/plus-gm.png' ?>" alt="GM" />
                 <img src="<?= get_template_directory_uri() . '/assets/img/Plus-Itausa.png' ?>" alt="ITAUSA" />
-                <img src="<?= get_template_directory_uri() . '/assets/img/plus-mz.png' ?>" alt="MZ" />
+                <img src="<?= get_template_directory_uri() . '/assets/img/plus-newU.png' ?>" alt="MZ" />
             </div>
         </div>
         <div class="row parceiros">
