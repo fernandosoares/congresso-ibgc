18dd47b0303ae1e7c059c017510f4fe8c00519dc
Fernando Soares <fernandosoares.dev@gmail.com>
2022-10-25 09:16:57 -0300

    Pop Up

---
+++ partials/page/modal.php
@@ -0,0 +1,10 @@
+<div id="modal" class="modal fade" tabindex="-1">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <button type="button" class="btn-close close-modal" data-bs-dismiss="modal" aria-label="Close"></button>
+            <div class="modal-body d-flex justify-content-center">
+                <img src="<?= get_template_directory_uri() . '/assets/img/poup-up-congresso-23.png' ?>" alt="">
+            </div>
+        </div>
+    </div>
+</div>
