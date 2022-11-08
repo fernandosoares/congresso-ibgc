7db58b040af41221edfcc10fdd3b31b9f4ec003c
Fernando Soares, O Dev <fernandosoares.dev@gmail.com>
2022-11-08 10:38:13 -0300

    Remove regra da barra

--- partials/page/countdown.php
+++ partials/page/countdown.php
@@ -1,8 +1,8 @@
 <section id="contador" class="py-4">
     <div class="container">
         <div class="row align-items-center">
-            <?php if (isBeforeEventStart()) : ?>
-                <div class="col-12 col-md-7">
+
+            <!-- <div class="col-12 col-md-7">
                     <div class="d-none d-md-flex">
                         <p class="starts">O evento começa em:</p>
                         <p class="day"></p>
@@ -36,38 +36,38 @@
                             Inscreva-se
                         </a>
                     </div>
+                </div> -->
+
+            <div class="col-12 col-md-7">
+                <div class="share d-flex justify-content-start">
+                    <p class="text-share">
+                        Acompanhe o Congresso: <span class="fw-normal">Acesse:</span>
+                    </p>
+                    <a href="https://congressoibgc.yazo.app.br/" target="_blank">
+                        <img src="<?= get_template_directory_uri() . '/assets/img/plataforma.png' ?>" alt="Plataforma" />
+                    </a>
+                    <a class="ms-4" href="https://onelink.to/gffx9v" target="_blank">
+                        <img src="<?= get_template_directory_uri() . '/assets/img/aplicativo.png' ?>" alt="Aplicativo" />
+                    </a>
                 </div>
-            <?php else : ?>
-                <div class="col-12 col-md-7">
-                    <div class="share d-flex justify-content-start">
-                        <p class="text-share">
-                            Acompanhe o Congresso: <span class="fw-normal">Acesse:</span>
-                        </p>
-                        <a href="https://congressoibgc.yazo.app.br/" target="_blank">
-                            <img src="<?= get_template_directory_uri() . '/assets/img/plataforma.png' ?>" alt="Plataforma" />
-                        </a>
-                        <a class="ms-4" href="https://congressoibgc.yazo.app.br/" target="_blank">
-                            <img src="<?= get_template_directory_uri() . '/assets/img/aplicativo.png' ?>" alt="Aplicativo" />
-                        </a>
-                    </div>
+            </div>
+            <div class="col-12 col-md-5">
+                <div class="share d-flex justify-content-end">
+                    <p class="text-share">
+                        Compartilhe:
+                    </p>
+                    <a href="https://www.linkedin.com/shareArticle?url=<?= urlencode('www.ibgc.org.br/congresso') ?>" target="_blank">
+                        <img src="<?= get_template_directory_uri() . '/assets/img/in.png' ?>" alt="Compartilhe no LinkedIn" />
+                    </a>
+                    <a href="https://wa.me/?text=Já garanti meu ingresso para o Congresso IBGC 2022! Vamos juntos? www.ibgc.org.br/congresso" target="_blank">
+                        <img src="<?= get_template_directory_uri() . '/assets/img/wpp.png' ?>" alt="Compartilhe no WhatsApp" />
+                    </a>
+                    <a class="inscreva" href="#ingressos">
+                        Inscreva-se
+                    </a>
                 </div>
-                <div class="col-12 col-md-5">
-                    <div class="share d-flex justify-content-end">
-                        <p class="text-share">
-                            Compartilhe:
-                        </p>
-                        <a href="https://www.linkedin.com/shareArticle?url=<?= urlencode('www.ibgc.org.br/congresso') ?>" target="_blank">
-                            <img src="<?= get_template_directory_uri() . '/assets/img/in.png' ?>" alt="Compartilhe no LinkedIn" />
-                        </a>
-                        <a href="https://wa.me/?text=Já garanti meu ingresso para o Congresso IBGC 2022! Vamos juntos? www.ibgc.org.br/congresso" target="_blank">
-                            <img src="<?= get_template_directory_uri() . '/assets/img/wpp.png' ?>" alt="Compartilhe no WhatsApp" />
-                        </a>
-                        <a class="inscreva" href="#ingressos">
-                            Inscreva-se
-                        </a>
-                    </div>
-                </div>
-            <?php endif; ?>
+            </div>
+
         </div>
     </div>
 </section>
